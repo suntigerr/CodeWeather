32a3c953699607912451943bcbbf40c36286820f
suntigerr <343195025@qq.com>
2018-05-07 21:07:45 +0800

    加入数据库和表的各项配置

---
+++ app/src/main/java/com/example/sun/codeweather/db/County.java
@@ -0,0 +1,44 @@
+package com.example.sun.codeweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    //县级天气
+    private String weatherId;
+    //所属城市
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
